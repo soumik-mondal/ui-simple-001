eb46204909c431354003e36b0435837f0f9dc5cd
soumik-mondal <64778804+soumik-mondal@users.noreply.github.com>
2025-08-06 17:12:37 +0530

    Update BasePage.java

--- src/main/java/com/intelli/automation/base/BasePage.java
+++ src/main/java/com/intelli/automation/base/BasePage.java
@@ -1,1 +1,102 @@
+package com.intelli.automation.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+/**
+ * Base page class following Intelli-Test framework
+ * Provides common page object functionality
+ */
+public abstract class BasePage {
+    
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+    
+    /**
+     * Wait for element to be clickable
+     * @param element WebElement to wait for
+     */
+    protected void waitForElementToBeClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+    
+    /**
+     * Wait for element to be visible
+     * @param element WebElement to wait for
+     */
+    protected void waitForElementToBeVisible(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+    
+    /**
+     * Click on element with wait
+     * @param element WebElement to click
+     */
+    protected void clickElement(WebElement element) {
+        waitForElementToBeClickable(element);
+        element.click();
+        System.out.println("🖱️ Clicked element: " + element.getText());
+    }
+    
+    /**
+     * Send keys to element with wait
+     * @param element WebElement to type in
+     * @param text Text to type
+     */
+    protected void sendKeysToElement(WebElement element, String text) {
+        waitForElementToBeVisible(element);
+        element.clear();
+        element.sendKeys(text);
+        System.out.println("⌨️ Typed text: " + text);
+    }
+    
+    /**
+     * Get text from element
+     * @param element WebElement to get text from
+     * @return Text content
+     */
+    protected String getElementText(WebElement element) {
+        waitForElementToBeVisible(element);
+        return element.getText();
+    }
+    
+    /**
+     * Check if element is displayed
+     * @param element WebElement to check
+     * @return true if displayed, false otherwise
+     */
+    protected boolean isElementDisplayed(WebElement element) {
+        try {
+            return element.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    /**
+     * Get page title
+     * @return Page title
+     */
+    public String getPageTitle() {
+        return driver.getTitle();
+    }
+    
+    /**
+     * Get current URL
+     * @return Current URL
+     */
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+} 
